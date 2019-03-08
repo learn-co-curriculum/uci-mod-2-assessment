9bf985efd213a52a6cf38130934c35fd806736d5
Steven G. Harms <steven.harms@flatironschool.com>
2019-03-08 15:53:24 -0500

    Convert to procedural code

--- lib/password_validator.rb
+++ lib/password_validator.rb
@@ -5,45 +5,36 @@ class PasswordValidator
   class NoSpecialCharactersError < Exception; end
   class PasswordTooShortError < Exception; end
 
-  attr_reader :required_length
-
-  def initialize(test_string, required_length=8)
-    @test_string = test_string
-    @required_length = required_length
+  def self.safe?(test_string, required_length=8)
+    raise ArgumentError unless test_string
+    has_one_lowercase?(test_string) &&
+    has_one_uppercase?(test_string) &&
+    has_one_number?(test_string) &&
+    has_one_special_character?(test_string) &&
+    is_long_enough?(test_string, required_length)
   end
 
-  def safe?
-    has_one_lowercase? &&
-    has_one_uppercase? &&
-    has_one_number? &&
-    has_one_special_character? &&
-    is_long_enough?
-  end
-
-  private
-
-  def has_one_lowercase?
-    raise NoLowerCaseLettersError unless @test_string.match(/[abcdefghijklmnopqrstuvwxyz]/)
+  def self.has_one_lowercase?(s)
+    raise NoLowerCaseLettersError unless s.match(/[abcdefghijklmnopqrstuvwxyz]/)
     true
   end
 
-  def has_one_uppercase?
-    raise NoUpperCaseLettersError unless @test_string.match(/[ABCDEFGHIJKLMNOPQRSTUVWXYZ]/)
+  def self.has_one_uppercase?(s)
+    raise NoUpperCaseLettersError unless s.match(/[ABCDEFGHIJKLMNOPQRSTUVWXYZ]/)
     true
   end
 
-  def has_one_number?
-    raise NoNumbersError unless @test_string.match(/[0123456789]/)
+  def self.has_one_number?(s)
+    raise NoNumbersError unless s.match(/[0123456789]/)
     true
   end
 
-  def has_one_special_character?
-    raise NoSpecialCharactersError unless @test_string.match(%r'[!#$%&()*+,-./:;<=>?@\[\]^_{|}~]')
+  def self.has_one_special_character?(s)
+    raise NoSpecialCharactersError unless s.match(%r'[!#$%&()*+,-./:;<=>?@\[\]^_{|}~]')
     true
   end
 
-  def is_long_enough?
-    raise PasswordTooShortError unless @test_string.length >= required_length
+  def self.is_long_enough?(s, required_length)
+    raise PasswordTooShortError unless s.length >= required_length
   end
-
 end
--- spec/password_generator_spec.rb
+++ spec/password_generator_spec.rb
@@ -5,60 +5,45 @@ describe "PasswordValidator" do
     expect { PasswordValidator }.not_to raise_error
   end
 
-  describe "when initialized" do
-    it 'must be given with testable string' do
-      expect { PasswordValidator.new }.to raise_error(ArgumentError)
+  describe  'PasswordValidator.safe? method' do
+    it 'exists' do
+      expect(PasswordValidator).to respond_to(:safe?)
     end
 
-    it 'must be OK when given a testable string' do
-      expect { PasswordValidator.new("bad_password") }.not_to raise_error
+    it 'throws an ArgumentError when it is not passed an argument' do
+      expect{ PasswordValidator.safe? }.to raise_error(ArgumentError)
     end
 
-    it 'must be OK when a non-default length is passed as parameter 2' do
-      expect { PasswordValidator.new("bad_password", 99) }.not_to raise_error
+    it 'throws no ArgumentError when it is passed a valid password but no minimum length option' do
+      expect{ PasswordValidator.safe?("Ba!1asdfasdfasdf") }.not_to raise_error
     end
-  end
 
-  describe "the safe? method tests the password on multiple strategies" do
     it 'fails with NoLowerCaseLettersError when there are no lower-case letters' do
-      expect { PasswordValidator.new("BAD_PASSWORD").safe? }.to raise_error(PasswordValidator::NoLowerCaseLettersError)
+      expect { PasswordValidator.safe?("BAD_PASSWORD").safe? }.to raise_error(PasswordValidator::NoLowerCaseLettersError)
     end
 
     it 'fails with NoLowerCaseLettersError when there are no upper-case letters' do
-      expect { PasswordValidator.new("bad_password").safe? }.to raise_error(PasswordValidator::NoUpperCaseLettersError)
+      expect { PasswordValidator.safe?("bad_password") }.to raise_error(PasswordValidator::NoUpperCaseLettersError)
     end
 
     it 'fails with NoNumbersError when there are no numbers' do
-      expect { PasswordValidator.new("Ba").safe? }.to raise_error(PasswordValidator::NoNumbersError)
+      expect { PasswordValidator.safe?("Ba") }.to raise_error(PasswordValidator::NoNumbersError)
     end
 
     it 'fails with NoSpecialCharactersError when there are no special characters' do
-      expect { PasswordValidator.new("Ba1").safe? }.to raise_error(PasswordValidator::NoSpecialCharactersError)
+      expect { PasswordValidator.safe?("Ba1") }.to raise_error(PasswordValidator::NoSpecialCharactersError)
     end
 
     it 'fails with PasswordTooShortError when the password is too short' do
-      expect { PasswordValidator.new("Ba1!").safe? }.to raise_error(PasswordValidator::PasswordTooShortError)
+      expect { PasswordValidator.safe?("Ba1!") }.to raise_error(PasswordValidator::PasswordTooShortError)
     end
 
     it 'does not fail with PasswordTooShortError when the password default length is short' do
-      expect { PasswordValidator.new("Ba1!", 0).safe? }.not_to raise_error
+      expect { PasswordValidator.safe?("Ba1!", 0) }.not_to raise_error
     end
 
     it 'succeeds with a strong password' do
-      expect { PasswordValidator.new("Ba1!asdfasdfasdf") }.not_to raise_error
+      expect { PasswordValidator.safe?("Ba1!asdfasdfasdf") }.not_to raise_error
     end
   end
-
-  describe "when initialized a valid password" do
-    it 'does expose the required_length attribute' do
-      v = PasswordValidator.new("Ba1!asdfasdfasdf")
-      expect(v.required_length).to eq(8)
-    end
-
-    it 'does not expose the test_string instance variable' do
-      v = PasswordValidator.new("Ba1!asdfasdfasdf")
-      expect { v.test_string }.to raise_error(NoMethodError)
-    end
-  end
-
 end
