490712b87a10e041eec643dcef40221bcf611eed
Steven G. Harms <steven.harms@flatironschool.com>
2019-03-22 16:15:57 -0400

    Make Ruby section procedural based

--- spec/password_generator_spec.rb
+++ spec/password_generator_spec.rb
@@ -1,87 +1,91 @@
 require 'spec_helper'
 
-describe "PasswordValidator" do
-  it 'class exists' do
-    expect { PasswordValidator }.not_to raise_error
+describe "Procedural Code" do
+  describe 'has_one_lowercase?' do
+    it 'returns true if the argument did have a lower-case letter' do
+      expect( has_one_lowercase?("TEsT") ).to eq(true)
+    end
+    it 'returns false if the argument does not have a lower-case letter' do
+      expect( has_one_lowercase?("TEST") ).to eq(false)
+    end
   end
 
-  describe  'PasswordValidator.safe? method' do
-    it 'exists' do
-      expect(PasswordValidator).to respond_to(:safe?)
+  describe 'has_one_uppercase?' do
+    it 'returns true if the argument does not have a upper-case letter' do
+      expect( has_one_uppercase?("TEST") ).to eq(true)
+    end
+    it 'returns false if the argument did have a upper-case letter' do
+      expect( has_one_uppercase?("test") ).to eq(false)
+    end
+  end
+
+  describe 'has_one_number?' do
+    it 'returns true if the argument does not have a number' do
+      expect( has_one_number?("TEST1") ).to eq(true)
+    end
+    it 'returns false if the argument did have a number' do
+      expect( has_one_number?("test") ).to eq(false)
+    end
+  end
+
+  describe 'has_one_special_character?' do
+    it 'returns true if the argument does have a special_character' do
+      expect( has_one_special_character?("TEST!") ).to eq(true)
+    end
+    it 'returns false if the argument did have a special_character' do
+      expect( has_one_special_character?("test") ).to eq(false)
+    end
+  end
+
+  describe 'is_long_enough?' do
+    it 'returns true if the argument is long enough for the specified length' do
+      expect( is_long_enough?("TESTTEST", 8) ).to eq(true)
     end
 
-    it 'throws an ArgumentError when it is not passed an argument' do
-      expect{ PasswordValidator.safe? }.to raise_error(ArgumentError)
+    it 'returns false if the argument is long enough for the specified length' do
+      expect( is_long_enough?("T", 8) ).to eq(false)
     end
 
-    it 'throws no ArgumentError when it is passed a valid password but no minimum length option' do
-      expect{ PasswordValidator.safe?("Ba!1asdfasdfasdf") }.not_to raise_error
+    it 'returns true if it is longer than the specified length' do
+      expect( is_long_enough?("test", 1) ).to eq(true)
+    end
+  end
+
+  describe 'safe? method' do
+    it 'exists and requires a password as argument but not a default length' do
+      expect{ safe?("TEST") }.not_to raise_error
     end
 
-    it 'fails with NoLowerCaseLettersError when there are no lower-case letters' do
-      expect { PasswordValidator.safe?("BAD_PASSWORD").safe? }.to raise_error(PasswordValidator::NoLowerCaseLettersError)
+    it 'exists and requires a password as argument but accepts an overriding length' do
+      expect{ safe?("TEST", 100) }.not_to raise_error
     end
 
-    it 'fails with NoLowerCaseLettersError when there are no upper-case letters' do
-      expect { PasswordValidator.safe?("bad_password") }.to raise_error(PasswordValidator::NoUpperCaseLettersError)
+    it 'returns false when there are no special characters' do
+      expect( safe?("Ba1") ).to eq(false)
     end
 
-    it 'fails with NoNumbersError when there are no numbers' do
-      expect { PasswordValidator.safe?("Ba") }.to raise_error(PasswordValidator::NoNumbersError)
+    it 'returns false when the password is too short' do
+      expect( safe?("Ba1!") ).to eq(false)
     end
 
-    it 'fails with NoSpecialCharactersError when there are no special characters' do
-      expect { PasswordValidator.safe?("Ba1") }.to raise_error(PasswordValidator::NoSpecialCharactersError)
+    it 'returns false when the password default overridden to short' do
+      expect( safe?("Ba1!", 0) ).to eq(true)
     end
 
-    it 'fails with PasswordTooShortError when the password is too short' do
-      expect { PasswordValidator.safe?("Ba1!") }.to raise_error(PasswordValidator::PasswordTooShortError)
+    it 'returns false when there is no number' do
+      expect( safe?("Ba!asdfasdfasdf") ).to eq(false)
     end
 
-    it 'does not fail with PasswordTooShortError when the password default length is short' do
-      expect { PasswordValidator.safe?("Ba1!", 0) }.not_to raise_error
+    it 'returns false when there is no upper-case-letter' do
+      expect( safe?("ba!asdfasdfasdf") ).to eq(false)
+    end
+
+    it 'returns false when there is no lower-case-letter' do
+      expect( safe?("BA!ASDFASDFASDF") ).to eq(false)
     end
 
     it 'succeeds with a strong password' do
-      expect { PasswordValidator.safe?("Ba1!asdfasdfasdf") }.not_to raise_error
-    end
-  end
-
-  describe 'subroutines are called by safe?' do
-    it 'and it calls has_one_lowercase? ' do
-      testpw = "TEST"
-      expect(PasswordValidator).to receive(:has_one_lowercase?).with(testpw)
-      expect PasswordValidator.safe?(testpw)
-    end
-
-    it 'and it calls has_one_uppercase? ' do
-      testpw = "Test"
-      expect(PasswordValidator).to receive(:has_one_uppercase?).with(testpw)
-      expect PasswordValidator.safe?(testpw)
-    end
-
-    it 'and it calls has_one_number? ' do
-      testpw = "Te1t"
-      expect(PasswordValidator).to receive(:has_one_number?).with(testpw)
-      expect PasswordValidator.safe?(testpw)
-    end
-
-    it 'and it calls has_one_special_character? ' do
-      testpw = "Te1t!"
-      expect(PasswordValidator).to receive(:has_one_special_character?).with(testpw)
-      expect PasswordValidator.safe?(testpw)
-    end
-
-    it 'and it calls is_long_enough? ' do
-      testpw = "Te1t!asfasdfasdf"
-      expect(PasswordValidator).to receive(:is_long_enough?).with(testpw, 8)
-      expect PasswordValidator.safe?(testpw)
-    end
-
-    it 'and it calls is_long_enough? when length is overridden' do
-      testpw = "Te1t!"
-      expect(PasswordValidator).to receive(:is_long_enough?).with(testpw, 2)
-      expect PasswordValidator.safe?(testpw, 2)
+      expect( safe?("Ba1!asdfasdfasdf") ).to eq(true)
     end
   end
 end
